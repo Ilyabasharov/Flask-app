0c806407f3c86fb1b6ceacb641c376ad4df57e18
Basharov Ilya <basharov.iv@phystech.edu>
2020-11-16 12:46:51 +0300

    add jinja2 support

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
+env = Environment(loader = FileSystemLoader('templates'))
 
 @app.route('/')
 def main():
-	return 'Привет'
-
+	return env.get_template('base.html').render(title = 'hello')
 
 if __name__ == '__main__':
 	app.run(debug=True)
